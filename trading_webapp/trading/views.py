bdd774f31f33fe820524d82881add287fc224d08
mark8889 <loci7425@gmail.com>
2025-05-22 15:40:22 +0100

    changes done

--- trading_webapp/trading/views.py
+++ trading_webapp/trading/views.py
@@ -95,7 +95,7 @@ def run_all(request):
     date_format = "%d/%m/%Y"
 
     order_file = framework_main(control, combinedForcast_folder_path, csvs_dictionary, PDM, date_format, aum, is_markov=False)
-    output_path = os.path.join(settings.BASE_DIR, 'DATA', 'order_folder', 'orders_new3.csv')
+    output_path = os.path.join(settings.BASE_DIR, 'DATA', 'order_folder', 'orders_new5.csv')
     order_file.to_csv(output_path)
 
     single_line()
